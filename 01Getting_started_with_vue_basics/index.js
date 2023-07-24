4b21ab93c22bb79e5d4a7263b0066ee0b73f7fd3
TSUNG-YU WU <48641178+orps2678@users.noreply.github.com>
2023-07-24 10:04:41 +0800

    v-bind HTML屬性中綁定data中的數據
    1.v-bind
    2.v-bind的縮寫只保留:

--- 01Getting_started_with_vue_basics/index.js
+++ 01Getting_started_with_vue_basics/index.js
@@ -1,11 +1,9 @@
 const app = Vue.createApp({
     data(){
         return{
-            name:"小名",
-            cool:["小綠", "小紅"],
-            xx:{
-                xxs:123
-            }
+            name:"小名",//data數據傳遞
+            yahoo_link:'https://tw.yahoo.com/',
+            google_link:'https://www.google.com.tw/?hl=zh_TW',//縮寫 HTML屬性中綁定data中的數據
         }
     }
 });
